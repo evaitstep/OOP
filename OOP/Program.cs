8104b72517f72baebcdc01fcf927bee2180cf85f
CSharpFri <CSharpFri@CLASS-2-06>
2017-11-03 20:52:15 +0100

    faeat(OOP) class Chata added

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,7 +10,18 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ahoj");
+            Dom mojDom = new Dom("Activ 1", "aktivni", "nie", 3, 70.9f, 35.8f);
+            mojDom.PoskytniInformacie();
+            Console.WriteLine("Percento zastavanej plochy je {0}", mojDom.ZistiVyuzitelnostZastavanejPlochy());
+
+            Dom susedovDom = new Dom("Activ 2", "aktivni", "nie", 3, 70.9f, 35.8f);
+            susedovDom.PoskytniInformacie();
+            Console.WriteLine("Percento zastavanej plochy je {0}", susedovDom.ZistiVyuzitelnostZastavanejPlochy());
+
+            Chata chata = new Chata("chata 1", "pasivna", "10", 50, 200.5f, 100, "ano a velka");
+            chata.PoskytniInformacie();
+
+            Console.ReadLine();
 
         }
     }
